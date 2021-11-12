84938b344ab20a24f0b1d944082a5ccb31f44950
haroonym <younas.h03@htlwienwest.at>
2021-11-12 14:53:09 +0100

    eigenes Icon verwendet

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
       theme_color: '#963484',
       background_color: '#28c2ff',
       icons: [
-        { src: './img/icons/employees_512x512.png', sizes: '512x512', type: 'image/png' },
-        { src: './img/icons/employees_512x512.png', sizes: '192x192', type: 'image/png' },
+        { src: './img/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+        { src: './img/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
       ],
     },
   },
